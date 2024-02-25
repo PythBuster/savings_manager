5b10545bfb35ce41b34e26d98402fec65e1f12e8
PythBuster <pythbuster@gmail.com>
2024-02-25 09:01:18 +0100

    feat: add model_validator to DBSettings to convert db_port str to int

--- src/custom_types.py
+++ src/custom_types.py
@@ -1,8 +1,9 @@
 """All custom types are located here."""
 
 from enum import StrEnum
+from typing import Any
 
-from pydantic import BaseModel, ConfigDict, SecretStr
+from pydantic import BaseModel, ConfigDict, SecretStr, model_validator
 
 
 class EndpointRouteType(StrEnum):
@@ -51,3 +52,17 @@ class DBSettings(BaseModel):
 
     model_config = ConfigDict(extra="forbid")
     """Model config."""
+
+    @model_validator(mode="before")
+    @classmethod
+    def convert_port(cls, data: dict[str, Any]) -> dict[str, Any]:
+        """Conert db_port as string to int.
+
+        :param data: The model data.
+        :type data: :class:`dict[str, Any]
+        :return: The updated model data:
+        :rtype: :class:`dict[str, Any]
+        """
+
+        data["db_port"] = int(data["db_port"])
+        return data
