7cbc06d830c14b4ad5d037bfbad51e3f1132c235
PythBuster <pythbuster@gmail.com>
2024-02-27 00:41:48 +0100

    doc: update swaggerUI documentation for transfer balance endpoint.

--- src/routes/moneybox.py
+++ src/routes/moneybox.py
@@ -118,7 +118,7 @@ async def delete_moneybox(
 async def deposit_moneybox(
     request: Request,
     moneybox_id: Annotated[
-        int, Path(ge=1, title="Moneybox ID", description="Moneybox ID to be updated.")
+        int, Path(ge=1, title="Moneybox ID", description="Moneybox ID where balance shall be added to.")
     ],
     deposit: Annotated[
         DepositModel,
@@ -141,7 +141,7 @@ async def deposit_moneybox(
 async def withdraw_moneybox(
     request: Request,
     moneybox_id: Annotated[
-        int, Path(ge=1, title="Moneybox ID", description="Moneybox ID to be updated.")
+        int, Path(ge=1, title="Moneybox ID", description="Moneybox ID where balance shall be withdrawn from.")
     ],
     withdraw: Annotated[
         WithdrawModel,
