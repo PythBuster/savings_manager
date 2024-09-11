5a50d85a9107c962c22e0e85181ac2b19dc53774
PythBuster <pythbuster@gmail.com>
2024-09-11 15:50:29 +0200

    feat: add docstrings to all classes

--- src/routes/exceptions.py
+++ src/routes/exceptions.py
@@ -5,5 +5,7 @@ class MissingSMTPSettingsError(Exception):
     """The MissingSMTPSettingsError class."""
 
     def __init__(self) -> None:
+        """Initialize the MissingSMTPSettingsError instance."""
+
         self.message = "SMTP settings incomplete. Can't send email."
         super().__init__(self.message)
