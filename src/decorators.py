867249e5a782471103b75409145d805462297c67
PythBuster <pythbuster@gmail.com>
2024-09-13 16:52:01 +0200

    feat: adapt BackgroundTaskRunner and implement decorator to decorate endless loop with sleeping of a specific time (async)

---
+++ src/decorators.py
@@ -0,0 +1,70 @@
+"""All custom decorators are located here."""
+
+import asyncio
+from functools import wraps
+from typing import Callable
+
+
+# decorator class
+class each:
+    """Task function decorator for functions used in BackgroundTaskRunner"""
+
+    @staticmethod
+    def hour(interval: int) -> Callable:
+        def decorator(func: Callable) -> Callable:
+            func_name = func.__name__.upper()  # Capture the name of the decorated function
+
+            @wraps(func)
+            async def wrapper(obj, *args, **kwargs):  # type: ignore
+                await obj.print_task(
+                    task_name=func_name,
+                    message="Task started ...",
+                )
+
+                while True:
+                    await func(obj, *args, **kwargs)
+                    await asyncio.sleep(interval * 3600)  # wait given interval in hours
+
+            return wrapper
+
+        return decorator
+
+    @staticmethod
+    def minute(interval: int) -> Callable:
+        def decorator(func: Callable) -> Callable:
+            func_name = func.__name__.upper()  # Capture the name of the decorated function
+
+            @wraps(func)
+            async def wrapper(obj, *args, **kwargs):  # type: ignore
+                await obj.print_task(
+                    task_name=func_name,
+                    message="Task started ...",
+                )
+
+                while True:
+                    await func(obj, *args, **kwargs)
+                    await asyncio.sleep(interval * 60)  # wait given interval in minutes
+
+            return wrapper
+
+        return decorator
+
+    @staticmethod
+    def second(interval: int) -> Callable:
+        def decorator(func: Callable) -> Callable:
+            func_name = func.__name__.upper()  # Capture the name of the decorated function
+
+            @wraps(func)
+            async def wrapper(obj, *args, **kwargs):  # type: ignore
+                await obj.print_task(
+                    task_name=func_name,
+                    message="Start task ...",
+                )
+
+                while True:
+                    await func(obj, *args, **kwargs)
+                    await asyncio.sleep(interval)  # wait given interval in seconds
+
+            return wrapper
+
+        return decorator
