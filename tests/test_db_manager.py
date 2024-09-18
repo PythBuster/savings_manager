49137c1a149231b9bb6df7e9441dc2b938bf84ec
PythBuster <pythbuster@gmail.com>
2024-09-18 15:45:45 +0200

    fix: fix/adapt test_export_sql_dump.py

--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -1196,4 +1196,4 @@ async def test_export_sql_dump(
     with dump_file_path.open("wb") as f:
         f.write(dump_stream.getvalue())
 
-    assert len(dump_stream.getvalue()) == 3120
+    assert 3100 < len(dump_stream.getvalue()) < 3130
