902000f870c5691d4673ace40a8a632e47a7360e
PythBuster <pythbuster@gmail.com>
2024-09-29 17:56:56 +0200

    #102 feat: adapt custom api schema creation and remove 401 and 403 from responses (SwaggerUI doc)

--- src/routes/responses/custom_openapi_schema.py
+++ src/routes/responses/custom_openapi_schema.py
@@ -4,10 +4,23 @@ from typing import Any
 
 from fastapi import FastAPI
 
+from src.custom_types import EndpointRouteType
+
 
 def custom_400_500_openapi_schema(fastapi_app: FastAPI) -> dict[str, Any]:
     if fastapi_app.openapi_schema:
         return fastapi_app.openapi_schema
+
+    # adapt sets to exclude paths in autom. response code generations
+    api_root = EndpointRouteType.APP_ROOT
+    app_root = EndpointRouteType.APP
+
+    exclude_401_for_paths: set[str] = {
+       f"/{api_root}/{app_root}/login",
+    }
+    exclude_403_for_paths: set[str] = {
+       f"/{api_root}/{app_root}/login",
+    }
 
     # call original openapi get the openapi schema
     original_openapi: dict[str, Any] = fastapi_app.openapi_original()  # type: ignore
@@ -44,33 +57,35 @@ def custom_400_500_openapi_schema(fastapi_app: FastAPI) -> dict[str, Any]:
                 },
             }
 
-            original_openapi["paths"][path][method]["responses"]["401"] = {
-                "description": "Unauthorized",
-                "content": {
-                    "application/json": {
-                        "example": {
-                            "summary": "Unauthorized",
-                            "value": {
-                                "message": "Unauthorized",
+            if path not in exclude_401_for_paths:
+                original_openapi["paths"][path][method]["responses"]["401"] = {
+                    "description": "Unauthorized",
+                    "content": {
+                        "application/json": {
+                            "example": {
+                                "summary": "Unauthorized",
+                                "value": {
+                                    "message": "Unauthorized",
+                                },
                             },
-                        },
-                    }
-                },
-            }
+                        }
+                    },
+                }
 
-            original_openapi["paths"][path][method]["responses"]["403"] = {
-                "description": "Forbidden",
-                "content": {
-                    "application/json": {
-                        "example": {
-                            "summary": "Forbidden",
-                            "value": {
-                                "message": "Forbidden",
+            if path not in exclude_403_for_paths:
+                original_openapi["paths"][path][method]["responses"]["403"] = {
+                    "description": "Forbidden",
+                    "content": {
+                        "application/json": {
+                            "example": {
+                                "summary": "Forbidden",
+                                "value": {
+                                    "message": "Forbidden",
+                                },
                             },
-                        },
-                    }
-                },
-            }
+                        }
+                    },
+                }
 
             original_openapi["paths"][path][method]["responses"]["500"] = {
                 "description": "Internal Server Error",
