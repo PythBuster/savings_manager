6ba34e9ce257bec0e13c3eed10c0f6fd1882df10
PythBuster <pythbuster@gmail.com>
2024-10-07 09:03:01 +0200

    build: adapt clean db backup script and keep 3 files + add cleanup step in backup.sh progress, which will run as cronjob

--- scripts/clean_up_postgres_database_backups.sh
+++ scripts/clean_up_postgres_database_backups.sh
@@ -2,4 +2,4 @@
 
 # todo: instead of 'postgres_backup' use env var!
 # keep last 3 backups and remove older backups
-ls -1t /postgres_backup/backup_*.sql | tail -n +3 | xargs -r rm --
+ls -1t /postgres_backup/backup_*.sql | tail -n +4 | xargs -r rm --
--- scripts/entrypoint_postgres_database_backup.sh
+++ scripts/entrypoint_postgres_database_backup.sh
@@ -4,18 +4,15 @@
 echo "postgres_database:5432:${POSTGRES_DB}:${POSTGRES_USER}:${POSTGRES_PASSWORD}" > ~/.pgpass && \
     chmod 600 ~/.pgpass
 
-# Create a script that runs pg_dump
-echo "#!/bin/bash\n pg_dump -Fc -h postgres_database -p 5432 -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f ${BACKUP_DIR}/backup_\$(date +%Y-%m-%d_%H%M).sql" > /backup.sh && \
-    chmod +x /backup.sh
+# Create a script that runs pg_dump and clean up old db backups
+echo "#!/bin/bash\n pg_dump -Fc -h postgres_database -p 5432 -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f ${BACKUP_DIR}/backup_\$(date +%Y-%m-%d_%H%M).sql" > /backup.sh
+echo "/bin/bash /clean_up_postgres_database_backups.sh" >> /backup.sh
+chmod +x /backup.sh
 
 # Add cron job to crontab for root
-echo "0 */12 * * * root /bin/bash /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/postgres_backup && \
 #echo "* * * * * root /bin/bash /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/postgres_backup && \
-    chmod 0644 /etc/cron.d/postgres_backup
-
-# Add cron job to crontab for root
-echo "* 12 * * 7 root /bin/bash /clean_up_postgres_database_backups.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/clean_postgres_backup && \
-#echo "* * * * * root /bin/bash /clean_up_postgres_database_backups.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/clean_postgres_backup && \
+#    chmod 0644 /etc/cron.d/postgres_backup
+echo "0 */12 * * * root /bin/bash /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/postgres_backup && \
     chmod 0644 /etc/cron.d/clean_postgres_backup
 
 exec "$@"
